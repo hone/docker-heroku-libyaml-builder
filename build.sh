c3d773a858d99ed0c9de4d6c7d04cec6a2cc5818
Terence Lee <hone02@gmail.com>
2014-03-27 12:08:59 -0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+workspace_dir=$1
+output_dir=$2
+
+artifact_dir="$workspace_dir/yaml-$VERSION"
+
+curl http://pyyaml.org/download/libyaml/yaml-$VERSION.tar.gz -s -o - | tar zxf -
+
+cd yaml-$VERSION
+env CFLAGS=-fPIC ./configure --enable-static --disable-shared --prefix=$artifact_dir
+make
+make install
+cd $artifact_dir
+tar czf $output_dir/libyaml-$VERSION.tgz *
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run -v `pwd`/builds:/tmp/output -e VERSION=0.1.6 hone/libyaml-builder
